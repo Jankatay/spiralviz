0ccdde4659367a717a682238e3ed602972eb6892
Jankatay <barjanay@proton.me>
2026-08-20 01:18:14 -0500

    iterating csv files portion done.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,11 @@
-use std::{
-    fs::File,
-    io::{self, BufRead, BufReader},
-};
+use std::io;
+
+pub mod spiralviz;
+use spiralviz::CsvIterator;
 
 fn main() -> io::Result<()> {
-    let target_file = File::open("./GLB.Ts+dSST.csv")?;
-    let target_file = BufReader::new(target_file);
-
-    // for each line in file
-    for line in target_file.lines() {
-        let line = line?;
-
-        // print the index-value pairs
-        for (i, value) in line.split(',').enumerate() {
-            print!("({i},{value}), ");
-        }
-        println!();
+    let mut csv_iterator = CsvIterator::new("./GLB.Ts+dSST.csv")?;
+    for elem in csv_iterator {
     }
-
-    // exit
-    return Ok(());
+    Ok(())
 }
